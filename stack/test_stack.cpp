d89f50fad996bf5638aa31dbf4a0ea7efc1141b8
stepin105599 <ragulkrish974@gmail.com>
2021-02-08 01:08:33 +0530

    Update test_stack.cpp

--- stack/test_stack.cpp
+++ stack/test_stack.cpp
@@ -1,7 +1,11 @@
 #include "stack.h"
 #include <gtest/gtest.h>
 namespace{
-
+    /**
+     * @brief Construct a new TEST object for push
+     * Pushing value into a stack
+     * 
+     */
     TEST(Stack, push_op){
         MyStack<int> c1;
         c1.push(111);
@@ -10,7 +14,11 @@ namespace{
         EXPECT_EQ(333,c1.peek());
         // EXPECT_EQ(0,c1.peek());
     }
-
+    /**
+     * @brief Construct a new TEST object for pop
+     * Poping up the pushed value
+     * 
+     */
     TEST(Stack, pop){
         MyStack<int> c1;
         c1.push(111);
@@ -19,7 +27,10 @@ namespace{
         EXPECT_EQ(222,c1.pop());
         // EXPECT_EQ(0,c1.peek());
     }
-
+    /**
+     * @brief Construct a new TEST object for peek
+     * 
+     */
     TEST(Stack, peek){
         MyStack<int> c1;
         c1.push(111);
@@ -28,7 +39,10 @@ namespace{
         EXPECT_EQ(333,c1.peek());
         // EXPECT_EQ(0,c1.peek());
     }
-
+    /**
+     * @brief Construct a new TEST object empty function
+     * 
+     */
     TEST(Stack, isEmpty){
         MyStack<int> c1;
         c1.push(111);
@@ -37,7 +51,10 @@ namespace{
         EXPECT_EQ(false,c1.isEmpty());
         // EXPECT_EQ(0,c1.peek());
     }
-
+    /**
+     * @brief Construct a new TEST object for checking  if stack is full
+     * 
+     */
     TEST(Stack, full){
         MyStack<int> c1;
         c1.push(111);
