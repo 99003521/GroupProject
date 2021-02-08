ea28840912a17dda31d75372c8fad3ffdbea32ff
adharsa mohan r <69114229+AdharsaMohanR@users.noreply.github.com>
2021-02-08 08:50:10 +0530

    Update point_testcase.cpp

--- point_testcase.cpp
+++ point_testcase.cpp
@@ -5,7 +5,7 @@ using namespace
     TEST(Point,Parameterised_constructor)
 {
     Point <int> p1(3,5);
-    EXPECT_EQ(1,p1.quadrant())
+    EXPECT_EQ(1,p1.quadrant());
     Point <int> p2(-2,9);
     EXPECT_EQ(2,P2.quadrant());
     Point <int> P3(-7,-1);
