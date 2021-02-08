c2cdcaf31d33df588344d070cf93aef7bdf1033e
stepin105599 <ragulkrish974@gmail.com>
2021-02-09 01:35:42 +0530

    Update test_currency.cpp

--- currency/test_currency.cpp
+++ currency/test_currency.cpp
@@ -7,8 +7,7 @@ TEST(Currency, Addition) {
   Currency t1(100,56);
   Currency t2(300,8);
   Currency t3=t1+t2;
-
-  EXPECT_EQ(400,t3.getRupee());
+` EXPECT_EQ(400,t3.getRupee());
   EXPECT_EQ(64,t3.getPaise());
 }
 }
