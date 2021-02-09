53f1c68ec72dab1f4d754383a9073f4b96d8a1d0
Adharsa Mohan R <adharsa1709@gmail.com>
2021-02-10 00:24:19 +0530

    updated

--- Library/library.cpp
+++ Library/library.cpp
@@ -1,4 +1,17 @@
-#include "library.h"
+
+/**
+ * @file library.cpp
+ * @author Aman Ajay Kalaskar
+ *         Aiswarya P S
+ *         Ragul Krishna 
+ *         Adharsa Mohan R
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<iostream>
 #include <fstream> 
 #include<vector>
@@ -9,13 +22,27 @@
 Library::Library(){}
 
 Library::Library(double i, string n):l_number(i),l_name(n){}
-
+/**
+ * @brief Add Function
+ * 
+ * @param ref 
+ */
 void Library::add(Book ref){
     V.push_back(ref);
 }
+/**
+ * @brief Remove Function
+ * 
+ * @param ref 
+ */
 void Library::remove(Book ref){
     // V.erase(ref);
 }
+/**
+ * @brief display function
+ * 
+ * @return string 
+ */
 string Library::display(){
     string str;
     for (int i=0;i<V.size(); ++i){
